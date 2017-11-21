a8e02bb25857bb4c31f42f421a0c10d6cb412fac
Matteo Troìa <matteotroia@icloud.com>
2017-11-21 14:27:25 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -46,8 +46,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Capitolo1'
-copyright = u'2017, Mattoe'
-author = u'Mattoe'
+copyright = u'2017, Matteo'
+author = u'Matteo'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
